8df4ff8dd75db5ed2d8dc2851ea15e55a726e31c
Roshan Subedi <everforgoodrs@gmail.com>
2021-10-02 15:13:53 +0545

    change on seettings

--- elibrary/settings.py
+++ elibrary/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[BASE_DIR/ 'static/']
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'#horeku
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'#horeku
 
 MEDIA_URL='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR/ 'media')
