ac2dc19a1bb285398ad3da5d59ace976bd51ba2e
Roshan Subedi <everforgoodrs@gmail.com>
2021-10-02 14:42:42 +0545

    static files colected

--- elibrary/settings.py
+++ elibrary/settings.py
@@ -86,8 +86,15 @@ WSGI_APPLICATION = 'elibrary.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+#for horeku
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
